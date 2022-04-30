97d307fd05bf7a37793a402181f5ab3a16470aaf
Sophie <me@soopy.moe>
2022-04-30 14:34:58 +0800

    feat: basic site design

--- src/index.js
+++ src/index.js
@@ -1,1 +1,33 @@
 import * as bootstrap from 'bootstrap';
+import {debug} from 'debug';
+
+localStorage.debug = '*'
+
+const mainLogger = debug("main");
+mainLogger("Hello");
+
+/**
+ * Initialize popper types
+ * @param {String} type - Popper type to load
+ * @returns {Array}
+ */
+function loadPoppers(type) {
+    const poppers = [].slice.call(document.querySelectorAll(`[data-bs-toggle="tooltip"][data-popper-type="${type}"]`));
+    mainLogger(`Found ${poppers.length} popper elements with type ${type}`)
+    const popperList = poppers.map(element => {
+        mainLogger(`Initializing popper element ${element}`)
+        return new bootstrap.Tooltip(element)
+    })
+}
+
+// console.log(
+//   'Build info:',
+//   new Date(process.env.BUILD_DATE),
+//   process.env.BUILD_VERSION,
+//   process.env.PACKAGE_VERSION,
+// )
+
+// if (module.hot) {
+//     module.hot.accept();
+// }
+loadPoppers("license");
