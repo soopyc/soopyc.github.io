42713e232b541bfd2a7f037f037565948d671f3e
Cassie Cheung <me@soopy.moe>
2023-10-28 00:20:56 +0800

    feat: actually use the icon plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import type { UserConfig } from 'vite';
+import Icons from "unplugin-icons/vite"
+import { defineConfig } from 'vite';
 
-const config: UserConfig = {
-	plugins: [sveltekit()]
-};
-
-export default config;
+export default defineConfig({
+    plugins: [
+        sveltekit(),
+        Icons({ compiler: "svelte" })
+    ]
+})
