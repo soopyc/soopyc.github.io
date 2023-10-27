e353679408b6a0630495abd793ca9cd47a918841
Cassie Cheung <me@soopy.moe>
2023-10-28 01:20:16 +0800

    fix: handle errors when failing to load discord user

--- src/routes/about/+page.ts
+++ src/routes/about/+page.ts
@@ -2,7 +2,12 @@ import type {PageLoad} from './$types';
 import sites from '$lib/constants/sites'
 
 export const load: PageLoad = async ({fetch}) => {
-    const discordData = await (await fetch(sites.lanyard.rest('397029587965575170'))).json()
+    let discordData: any = {data: {discord_user: {}, kv: {}}};  // FIXME: we had to use `any` here to silence ts warnings.
+    try {
+        discordData = await (await fetch(sites.lanyard.rest('397029587965575170'))).json()
+    } catch (e) {
+        console.error('could not load discord user, bailing.')
+    }
     return {
         discordUser: discordData.data.discord_user,
         lanyardkv: discordData.data.kv
