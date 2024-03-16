c0d0c32e64e2a950207184732af6bf46d0f814b4
Cassie Cheung <me@soopy.moe>
2024-03-16 16:27:18 +0800

    feat!: move to cloudflare pages

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 // import adapter from '@sveltejs/adapter-auto';
-import adapter from '@sveltejs/adapter-netlify';
+import adapter from '@sveltejs/adapter-cloudflare';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
