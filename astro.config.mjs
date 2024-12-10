7f9d4f53f7ef24cdb7f523d93058eebd4fcc401f
Cassie Cheung <me@soopy.moe>
2024-12-10 23:57:19 +0800

    feat: init some content

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,9 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import cloudflare from '@astrojs/cloudflare';
-
-import tailwind from '@astrojs/tailwind';
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-  adapter: cloudflare(),
-  integrations: [tailwind()]
+    adapter: cloudflare(),
 });
