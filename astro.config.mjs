487f923972930599fab1d89feca578cb8ed19508
Cassie Cheung <me@soopy.moe>
2024-12-13 23:35:03 +0800

    deps: add astro-icon

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
+import icon from "astro-icon";
 import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
     adapter: cloudflare(),
+    integrations: [icon()],
 });
