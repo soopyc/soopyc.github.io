91f027d8c1195d47122303ca1fe824d32c57af44
Cassie Cheung <me@soopy.moe>
2024-12-16 17:49:36 +0800

    feat: add h-entry to blog posts

--- src/assets/constants.ts
+++ src/assets/constants.ts
@@ -1,5 +1,13 @@
+interface Author {
+    url: string,
+}
+
 export const avatar = "https://www.libravatar.org/avatar/58a08528a7e2b3fd738b25d0c63f5c82?s=512";
+export const authors: Record<string, Author> = {
+    soopyc: {url: "https://soopy.moe"},
+};
 
 export default {
     avatar,
+    authors,
 }
